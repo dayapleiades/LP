2d554a89c6a7fc54f0daa1885165638b3f64c3d8
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2021-12-07 18:11:16 -0300

    Create SomaSimples.c

---
+++ LP/SomaSimples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  // Inclusão de biblioteca
+#include <math.h> 
+
+int main() // Programa principal
+{
+    int A, B, SOMA; // Declação de variáveis
+    scanf ("%d", &A); // Leitura de variáveis
+    scanf ("%d", &B); // Leitura de variáveis
+    SOMA=A+B;  // Cálculo de soma
+    printf("SOMA = %d\n",SOMA); // Mostra o Resultado
+    return 0;
+}
