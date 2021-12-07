0f40255360ab3fac2295f182cd74671abc519434
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2021-12-07 17:57:25 -0300

    Create ExtremamenteBásico.c

---
+++ LP/ExtremamenteBásico.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Inclusão de bibliotecas
+ 
+int main() { // Programa principal
+ 
+    int A, B, X; // Declaração de variáveis
+    scanf ("%d", &A); // Leitura de variáveis 
+    scanf ("%d", &B); // Leitura de variáveis
+    X = A + B; // Soma de duas variáveis
+    printf("X = %d\n", X); // Mostra o Resultado
+    return 0;
+}
