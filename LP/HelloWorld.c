9ca0e785247db3b69cc4bad20c50a5308397f1fb
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2021-12-07 17:52:16 -0300

    Create HelloWorld.c

---
+++ LP/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Inclusão de biblioteca
+#include <math.h>
+
+int main() // Programa principal
+{
+   
+                printf("Hello World!\n"); // Mensagem
+      
+    return 0;
+}
