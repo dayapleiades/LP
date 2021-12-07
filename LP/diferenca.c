3e6d4dea7bf595e461de21d52ce9b68585cb407d
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2021-12-07 18:23:37 -0300

    Create diferenca.c

---
+++ LP/diferenca.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Inclusão de bibliotecas
+#include <math.h>
+
+int main() // Programa principal
+{
+     int A, B, C, D, DIFERENCA; // Declaração de variáveis
+     scanf ("%d %d %d %d", &A, &B, &C, &D); // Leitura de variáveis
+     DIFERENCA = (A*B-C*D); // Cálculo da diferença
+            printf("DIFERENCA = %d\n",DIFERENCA); // Mostra a diferença
+    return 0;
+}
