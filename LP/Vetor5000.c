037247c066078399fb6ff7a587efa1663b7803ad
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2021-12-16 16:13:39 -0300

    Update Vetor5000.c

--- LP/Vetor5000.c
+++ LP/Vetor5000.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main()
-{
+int main() {
     int *vetor; // Declaração do vetor
     int i; // Declaração de contador
     vetor = (int*)malloc(5000*sizeof(int)); // Alocando espaço para contador de 5000 posições
-        for (i = 0; i < 5000; i++){  // Contador
+    for (i = 0; i < 5000; i++) {  // Contador
         vetor[i]=i;
-        }
-        for (i = 0; i < 5000; i++){ // Contador
+    }
+    for (i = 0; i < 5000; i++) { // Contador
         printf("%d ", vetor[i]); // Exibe os valores de 0 até 4999
-        }
+    }
     free(vetor); // Libera a memória alocada
     return 0;
 }
