f0b47408e9745440f67c09f87f915885d6dc1e6c
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2022-01-17 19:00:55 -0300

    Create distancia.c

---
+++ LP/distancia.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> //inclusão de bibliotecas
+ 
+int main() { //programa principal
+ 
+    int min,D; //declaração da variável 
+    scanf("%d", &D); //leitura da variavel 
+    min = D*2; // calcula o tempo 
+    printf("%d minutos\n",min);//imprime o valor do tempo
+    return 0;
+}
