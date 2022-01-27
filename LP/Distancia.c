72014ee00f1dea89ff165f181e8fcc460a1fb5ea
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2022-01-27 20:04:19 -0300

    Update and rename distancia.c to Distancia.c

---
+++ LP/Distancia.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> // Inclusão de bibliotecas
+ 
+int main() { // Programa principal
+ 
+    int min,D; // Declaração da variável 
+    scanf("%d", &D); // Leitura da variavel 
+    min = D*2; // Calcula o tempo 
+    printf("%d minutos\n",min);// Imprime o valor do tempo
+    return 0;
+}
