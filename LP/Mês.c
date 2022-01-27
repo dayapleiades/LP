f6ddc10f8e3b30bcdc158d4da49f0638364157da
Daiane Moura da Silva <91854395+dayapleiades@users.noreply.github.com>
2022-01-27 19:45:10 -0300

    Create Mês.c

---
+++ LP/Mês.c
@@ -0,0 +1,44 @@
+#include <stdio.h> // Inclusão de bibliotecas
+ 
+int main() { // Programa principal
+ 
+    int mes; // Declarando variavel inteiro
+    scanf("%d", &mes); // Leitura da variavel mes
+    if (mes == 1) { // Se a variavel mes for igual a 1 
+        printf("January\n"); // Imprime o mes de janeiro
+    }
+    else if (mes == 2) { // Se a variavel mes for igual a 2
+        printf("February\n"); // Imprime o mes de Fevereiro
+    }
+    else if (mes == 3) { // Se a variavel mes for igual a 3
+        printf("March\n"); // Imprime o mes de Março
+    }
+    else if (mes == 4) { // Se a variavel mes for igual a 4
+        printf("April\n"); // Imprime o mes de Abril
+    }
+    else if (mes == 5) { // Se a variavel mes for igual a 5
+        printf("May\n"); // Imprime o mes de Maio
+    }
+    else if (mes == 6) { // Se a variavel mes for igual a 6
+        printf("June\n"); // Imprime o mes de junho
+    }
+    else if (mes == 7) { // Se a variavel mes for igual a 7
+        printf("July\n"); // Imprime o mes de julho
+    }
+    else if (mes == 8) { // Se a variavel mes for igual a 8
+        printf("August\n"); // Imprime o mes de Augusto
+    }
+    else if (mes == 9) { // Se a variavel mes for igual a 9
+        printf("September\n"); // Imprime o mes de Setembro
+    }
+    else if (mes == 10) { // Se a variavel mes for igual a 10
+        printf("October\n"); // Imprime o mes de Outubro
+    }
+    else if (mes == 11) { // Se a variavel mes for igual a 11
+        printf("November\n"); // Imprime o mes de Novembro
+    }
+    else if (mes == 12) { // Se a variavel mes for igual a 12
+        printf("December\n"); // Imprime o mes de Dezembro
+    }
+    return 0;
+}
